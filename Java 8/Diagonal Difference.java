10e993f4f40117f8dd6a15cef8f10961ef639ab2
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-05-12 09:33:08 -0500

    Diagonal Difference

---
+++ Java 8/Diagonal Difference.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the diagonalDifference function below.
+     */
+    static int diagonalDifference(int[][] a) {
+        int diagnol1 = 0;
+        int diagnol2 = 0;
+        //sum first diagnol
+        int i = 0;
+        for(int[]arItem : a){
+            diagnol1+=arItem[i++];
+        }
+        //sum opposite diagnol
+        for(int[]arItem : a){
+            diagnol2+=arItem[--i];
+        }
+        //return the difference & abosulte value of the two diagnols
+        return Math.abs(diagnol1-diagnol2);
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[][] a = new int[n][n];
+
+        for (int aRowItr = 0; aRowItr < n; aRowItr++) {
+            String[] aRowItems = scan.nextLine().split(" ");
+
+            for (int aColumnItr = 0; aColumnItr < n; aColumnItr++) {
+                int aItem = Integer.parseInt(aRowItems[aColumnItr].trim());
+                a[aRowItr][aColumnItr] = aItem;
+            }
+        }
+
+        int result = diagonalDifference(a);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+    }
+}
