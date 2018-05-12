0198d5c28de1b393b3ea25fd705b49fd3b17ede9
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-05-12 08:54:01 -0500

    Compare the triplets

---
+++ Compare the Triplets.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the solve function below.
+     */
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2) {
+        //create new array
+        int arr[] = new int[2];
+        arr[0] = 0;
+        arr[1] = 0;
+        //compare and add points
+        if(a0>b0)
+            arr[0]++;
+        else if(a0<b0)
+            arr[1]++;
+        if(a1>b1)
+            arr[0]++;
+        else if(a1<b1)
+            arr[1]++;
+        if(a2>b2)
+            arr[0]++;
+        else if(a2<b2)
+            arr[1]++;
+        
+        //return the points
+        return arr;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] a0A1A2 = scan.nextLine().split(" ");
+
+        int a0 = Integer.parseInt(a0A1A2[0].trim());
+
+        int a1 = Integer.parseInt(a0A1A2[1].trim());
+
+        int a2 = Integer.parseInt(a0A1A2[2].trim());
+
+        String[] b0B1B2 = scan.nextLine().split(" ");
+
+        int b0 = Integer.parseInt(b0B1B2[0].trim());
+
+        int b1 = Integer.parseInt(b0B1B2[1].trim());
+
+        int b2 = Integer.parseInt(b0B1B2[2].trim());
+
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write(" ");
+            }
+        }
+
+        bw.newLine();
+
+        bw.close();
+    }
+}
